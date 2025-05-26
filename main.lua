fadaded99152535e9c3b489f0f93ebdb9d6289e3
Water075 <yeahidkorb@gmail.com>
2025-05-26 17:30:32 +0100

    main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "AbilityWarsFucker", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "AbilityWarsFucker", HidePremium = false, SaveConfig = false, ConfigFolder = "wooow",IntroText = "loading sexy script :3"})
