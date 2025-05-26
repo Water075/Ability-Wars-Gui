94492b850dbb8f502770528b894a0ac8c5c252cb
Water075 <yeahidkorb@gmail.com>
2025-05-26 17:32:46 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,13 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "AbilityWarsFucker", HidePremium = false, SaveConfig = false, ConfigFolder = "wooow",IntroText = "loading sexy script :3"})
+
+-- MAIN SHIT BRO
+local Tab = Window:MakeTab({
+	Name = "main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = 1:AddSection({
+	Name = "Blatant Stuff (Admins are somewhat active be careful"
+})
